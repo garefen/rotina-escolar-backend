3cf6780b088f9d6d58d4203112c2533689cb1130
Gabriel Sarti <gabriel.sarti@rga.com>
2020-02-18 14:41:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv/config');
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const EraseOldAppointmentsController = require('./controllers/EraseOldAppointmentsController');
+
+const schedule = require('node-schedule');
+
+schedule.scheduleJob('00 00 * * *', EraseOldAppointmentsController.index);
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+app.use(routes);
+
+
+app.listen(3333);
